6400c8a6d41efc3cdf084e3bbd9a892357bfacb2
NicEastvillage <nicoesterby@gmail.com>
2019-06-02 22:02:26 +0200

    Is now a Discord Bot.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,53 +5,98 @@ import dk.eastvillage.dost.antlr.DostParser
 import dk.eastvillage.dost.ast.BuildAstVisitor
 import dk.eastvillage.dost.contextual.ContextualAnalysisVisitor
 import dk.eastvillage.dost.interpreter.Interpreter
+import net.dv8tion.jda.core.JDABuilder
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 import java.lang.RuntimeException
 
-fun main() {
-    println("Hello world")
 
-    val source = """
-        |var a = 5
-        |if (a > 0) {
-        |   a = a * 2 * (a - 1 + 1)
-        |   var a = 0
-        |}
-        |for (i in 0 ..= a) {
-        |   while (i == -1.0) { }
-        |}
-        |var b = (a != (0)) && true
-        |print a
-        |print b
-    """.trimMargin()
-
-    tryCompile(CharStreams.fromString(source))
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Needs one argument (bot token)")
+    } else {
+        val jda = JDABuilder(args[0]).build()
+        jda.addEventListener(MessageListener())
+    }
 }
 
-fun tryCompile(source: CharStream) {
+class MessageListener : ListenerAdapter() {
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        println("Msg received: " + event.message.contentRaw)
+        if (!event.author.isBot) {
+            val msg = event.message.contentRaw
+            if (msg.startsWith("-dost ") || msg.startsWith("-dost\n")) {
+                /* potential message:
+                -dost
+                ```
+                print 2 + 3
+                ```
+                 */
+
+                val pieces = msg.split("```")
+                if (pieces[0].matches(Regex("-dost.*\n"))) {
+                    val args = pieces[0].drop(5).split(" ").filter { it != "" }.map { it.trim() }
+                    println("Args: $args")
+                    val source = pieces[1].trim()
+
+                    // Parse arguments
+                    var pretty = false
+
+                    if (args[0] == "pretty") {
+                        pretty = true
+                    }
+
+                    // Run compiler
+                    if (pretty) {
+                        val result = tryCompileToString(source, true)
+                        event.channel.sendMessage("This is how I understand that piece of code:\n```$result```").queue()
+
+                    } else {
+                        val result = tryCompileToString(source, false)
+                        event.channel.sendMessage("```$result```").queue()
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun tryCompileToString(source: String, pretty: Boolean = false): String {
+    val baos = ByteArrayOutputStream()
+    PrintStream(baos, true, "UTF-8").use {
+            out -> tryCompile(CharStreams.fromString(source), out, pretty)
+    }
+    return String(baos.toByteArray())
+}
+
+fun tryCompile(source: CharStream, out: PrintStream = System.out, pretty: Boolean = false) {
     try {
-        compile(source)
+        compile(source, out, pretty)
 
     } catch (e: TerminatedCompilationException) {
 
-        System.err.println("Compilation failed!")
-        System.err.println("Printing errors (${ErrorLog.allErrors().size}) and warnings (${ErrorLog.allWarnings().size}) ...")
+        out.println("Compilation failed!")
+        out.println("Printing errors (${ErrorLog.allErrors().size}) and warnings (${ErrorLog.allWarnings().size}) ...")
         ErrorLog.printAllErrors()
         ErrorLog.allErrors()
 
     } catch (e: RuntimeException) {
 
-        System.err.println("Compiler crashed!")
-        System.err.println("Printing errors (${ErrorLog.allErrors().size}) and warnings (${ErrorLog.allWarnings().size}) ...")
+        out.println("Compiler crashed!")
+        out.println("Printing errors (${ErrorLog.allErrors().size}) and warnings (${ErrorLog.allWarnings().size}) ...")
         ErrorLog.printAllErrors()
         ErrorLog.allErrors()
         e.printStackTrace()
     }
 }
 
-fun compile(source: CharStream) {
+fun compile(source: CharStream, out: PrintStream, pretty: Boolean) {
 
     // Syntactical analysis
     val lexer = DostLexer(source)
@@ -60,13 +105,15 @@ fun compile(source: CharStream) {
     val ast = BuildAstVisitor().visit(parser.start())
     ErrorLog.assertNoErrors()
 
-    println("~~~~~~~~~~~ Pretty printer ~~~~~~~~~~~~~")
-    PrettyPrinter.print(ast, System.out)
+    if (pretty) {
+        PrettyPrinter.print(ast, out)
+        return
+    }
 
     // Contextual analysis
     ContextualAnalysisVisitor.analyse(ast)
     ErrorLog.assertNoErrors()
 
-    println("~~~~~~~~~ Interpreter output ~~~~~~~~~~~")
-    Interpreter(System.out).start(ast)
+    // Interpret
+    Interpreter(out).start(ast)
 }
